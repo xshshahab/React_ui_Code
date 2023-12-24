50315bf27b6dc02d5999bddeb2f1facd689f504e
Shanu <mdshahbuddin9504@gmail.com>
2023-12-25 04:06:49 +0530

    Connect appwrite & .env file, I have also use multiple package in this Directories

---
+++ ui_main-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>A Project With an AppWrite</h1>
+    </>
+  )
+}
+
+export default App
