010064ecdf7a75d1168000409201ef5e3358ee18
Shanu <mdshahbuddin9504@gmail.com>
2023-12-25 15:34:54 +0530

    Build Auth using Appwrite

--- ui_main-project/README.md
+++ ui_main-project/README.md
@@ -1,8 +1,7 @@
-# React + Vite
+# Today - Add Appwrite for using Backend 
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+- Currently, Add multiple plugins.
+- I update here : react-redux, react-router-dom, appwrite-npm, tinymce-react
+, html-react-parser
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# Today - Build Authentication using Appwrite
---
+++ ui_main-project/src/appwrite/auth.js
@@ -0,0 +1,55 @@
+import Config from '../Config/Config';
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client.setEndpoint(Config.appwriteUrl).setProject(Config.appwriteProjectId);
+
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // Call another method
+                return this.login({email, password});
+            } 
+        } catch (error) {
+            return userAccount;         
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async logout() {
+        try {
+            return this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+        return null;
+    }
+}
+
+const authServ = new AuthService();
+
+export default authServ;
