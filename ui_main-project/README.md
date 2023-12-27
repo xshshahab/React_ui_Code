3a817a6040d88c554f5818b5f17ac205152c1f4a
Shanu <mdshahbuddin9504@gmail.com>
2023-12-27 15:19:19 +0530

    Update file and add comments in readme file

--- ui_main-project/README.md
+++ ui_main-project/README.md
@@ -8,3 +8,9 @@
 
 - Currently, Add Auth file create more file like conf file. 
 - At the end of conf file. I create multiple services related to db and storage and custom queries.
+
+## Currently - Configure Redux
+- Basically, It means, I'll manage state from redux toolkit.
+- Today, I create components, pages, store - {for redux}.
+- create a two components like Header Footer and also update App file there i use outlet components from react-router-dom.
+- I'll manage my security login logout to store components.
--- ui_main-project/src/App.jsx
+++ ui_main-project/src/App.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Footer, Header } from './components/index';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const App = () => {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A Project With an AppWrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    }).finally(() => setLoading(false))
+  }, []);
+  
+
+  return !loading ? (
+    
+    <div className='bg-gray-700 min-h-screen flex flex-wrap content-between'>
+       <div className="w-full block">
+        <Header />
+          <main>
+            TODO : <Outlet />
+          </main>
+        <Footer />
+       </div>
+    </div>
+
+  ) : null
 }
 
 export default App
